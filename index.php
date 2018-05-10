da56568e3eb78f565736c26a13039010f60214e8
tamusN <37118679+tamusN@users.noreply.github.com>
2018-05-09 21:52:58 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <!--
 File Name: index.html
-Date: 5/3/18
+Date: 5/9/18
 Programmer: Tam Nguyen
 -->
 
@@ -29,20 +29,12 @@ Programmer: Tam Nguyen
 </head>
 
 <body>
- 
 <header class="header">
-  <input class="menu-btn" type="checkbox" id="menu-btn" />
-  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-  <h1 id="logo">H1 heading</h1>
-  <ul class="menu">
-     <li><a href="#intro" target="_self">Home</a></li>
-     <li><a href="#funfacts" target="_self">Events</a></li>
-      <li><a href="#funfacts" target="_self">FAQ</a></li>
-     <li><a href="#common" target="_self">Registration</a></li>
-     <li><a href="#references" target="_self">Contact</a></li>
-  </ul>
-</header>
+    
+<?php include 'includes/header.inc.html.php'; ?>
 
+    
+</header>
 <main>
 
 <section >
