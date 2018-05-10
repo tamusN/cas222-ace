14353357002dcbfe16d38a776c2d6f568815ff6f
tamusN <37118679+tamusN@users.noreply.github.com>
2018-05-09 21:45:23 -0700

    Create header.inc.html.php

---
+++ includes/header.inc.html.php
@@ -0,0 +1,14 @@
+<header class="header">
+
+ <input class="menu-btn" type="checkbox" id="menu-btn" />
+  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+  <h1 id="logo">H1 heading</h1>
+
+    <ul class="menu">
+     <li><a href="#intro" target="_self">Home</a></li>
+     <li><a href="#funfacts" target="_self">Events</a></li>
+      <li><a href="#funfacts" target="_self">FAQ</a></li>
+     <li><a href="#common" target="_self">Registration</a></li>
+     <li><a href="#references" target="_self">Contact</a></li>
+  </ul>
+</header>
