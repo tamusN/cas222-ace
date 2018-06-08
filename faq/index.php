59eda003b8cf306b87d2e28eb00940a1aca8c335
tamusN <37118679+tamusN@users.noreply.github.com>
2018-06-07 22:20:56 -0700

    Add files via upload

--- faq/index.php
+++ faq/index.php
@@ -1,1 +1,117 @@
+<!DOCTYPE html>
 
+
+<!--
+File Name: faq.php
+Date: 5/9/18
+Programmer: Tam Nguyen
+-->
+
+<html lang="en">
+
+<head>
+    
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8">
+<meta name="author" content="your name here">
+    
+<base target="_blank"> <!-- open all links not marked "_self" in a new tab -->
+
+<title>ACE</title>
+
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700' rel='stylesheet' type='text/css'>
+
+<link href="css/reset.css" rel="stylesheet" type="text/css">
+        
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+<link href="../css/custom.css" rel="stylesheet" type="text/css">
+    
+    
+<link href="../css/tpgrid.css" rel="stylesheet" type="text/css">
+    
+<link href="../css/tpstyle.less" rel="stylesheet/less" type="text/css">
+    
+<link href="../css/tpnavigation.css" rel="stylesheet" type="text/css">
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.min.js"></script>
+
+</head>
+
+<body>
+
+<header class="header">
+
+
+<?php include '../includes/header.inc.html.php'; ?>
+
+    
+</header>
+
+<main>
+
+
+<section class="section group">
+
+    
+<div class="col span_1_of_2">
+<img src="images/bike_man_1000x2.jpg" alt=""/>
+
+<h3>Frequently Asked Questions</h3>
+
+    <p class="question">What are the Rules?</p>
+    <p>We currently adhere to the USAT Rules for Triathlon & Duathlon.  Important rules include no drafting, you must wear a helmet and music is ABSOLUTELY NOT allowed during the bike for obvious safety reasons.</p>
+
+    <p class="question">Can I use a personal music device while cycling? </p>
+    <p>Absolutely no music devices are allowed during the bike segment for obvious safety risks and will result in immediate disqualification.</p>
+
+    <p class="question">Can I use a personal music device while running?</p> 
+    <p>During any running segments, we prefer for athletes to NOT use music devices to maximize safety, assure a fair competitive environment and assure athletes can hear all course marshal instructions. With that said, we also understand that many athletes rely on music to help endure the challenge of running. To accommodate the needs of our athletes while still assuring maximum safety and a fair, competitive environment, here is the arrangement that we can accommodate.<br>
+
+    If you chose to use a personal music device, you must always keep one ear open for instructions so only one ear-bud can be used at any time. <br>
+        In addition, if you opt to use music, you will not be eligible for awards, points and rankings. So as an athlete, you have to make a choice…music or awards/points/rankings.<br>
+    If you are using a personal music devise with two earbuds, you will be immediately disqualified. Trust us, we don’t want to have to enforce a disqualification but in order to assure the safety of all athletes, this is a very important rule.<br>
+    We also request that if you chose to use a personal music device that you keep the volume to a minimum to assure all instructions can be heard.
+
+
+    <p class="question">Do I need to wear a wetsuit? </p>
+    <p>No, you do not need to wear a wetsuit. Many will not wear a wetsuit while others will choose to wear a wetsuit because of the buoyancy and warmth factor. </p>
+    <p class="question">Do I have to use a road or racing bike? </p> 
+    <p>No. We welcome any type of bike as long as it is functioning properly with brakes and endcaps at the end of your handlebars.</p>
+
+
+
+</div >
+
+<div class="col span_1_of_2">
+    
+<h3>What To Bring With You </h3>
+
+<p>Watch the weather closely.  The show goes on no matter what the weather is doing. <br>
+
+You must wear your chip timing piece during the entire event.  <br><br>
+You will be given a band that will hold your chip timing piece around your ankle throughout the entire event. <br><br>
+    Be sure that it is snapped tightly. Be sure to have your Chip Timing piece on before you start the race and be sure to step over the timing mats after each segment of the race. If you are wearing a wet-suit, make sure the timing piece goes under your wetsuit otherwise, you will have a very difficult time getting off your wet suit.<br><br>
+
+<strong>Swim</strong>:  Wetsuits are optional for the swim but will provide buoyancy and warmth. However, many people opt for no wetsuit for a triathlon so no worries.  We will provide you with a swim cap but you will want to bring your own goggles.<br><br>
+
+<strong>Bike</strong>:  A biking helmet is mandatory.  You will also receive 2 stickers in your package with your race number on them.  The small sticker should go on the front of your helmet.  The bigger sticker will wrap around your bike frame.  Road or mountain bikes are acceptable.  For safety reasons, be sure to have end-caps on the end of your handle-bars.<br><br>
+
+<strong>Run</strong>:  You must finish the race with your bib number on the front of you.  Some people choose to pin it on at the beginning of the race and have it on for the whole event so they don’t have to worry about it.  Others pin it on a singlet that they put on once they finish the swim before they head out for the bike.  Others use an elastic racing strap that they pin their bib number to and then quickly strap it on before they leave for the run.  Use whatever option feels best for you.<br><br>
+
+Remember to bring a change of clothing so you can enjoy the post-event festivities. 
+</p> 
+</div>
+    
+</section>
+
+    
+</main>
+
+<?php include '../includes/footer.inc.html.php'; ?>
+
+    
+</body>
+
+</html>
